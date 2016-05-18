12314b1f08ba30db71abbcb2f4124fe7c5f555fb
Paul Gellerman <paul.c.gellerman.civ@mail.mil>
2016-05-18 21:06:17 +0000

    added more slides to presentation and more helper functions to hilbert main

--- hilbertmaker.R
+++ hilbertmaker.R
@@ -1,29 +1,39 @@
 library(ggplot2)
 library(iptools)
 
-Hilbert <- function(){
+clearHilbert <- function(){
     xlist <<- {}
     ylist <<- {}
 }
+calcHilbert <- function(){
+    #How many points are you trying to plot
+}
 
-hilbert <- function(x,y,xi,xj,yi,yj,n){
-    print("Hilbert function called")
+hilbert <- function(x0,y0,xi,xj,yi,yj,n){
+   # cat("Hilbert function called\n")
     if (n <= 0){
-        print("Point Calculated")
-        x0 <- (x +(xi+yi)/2)
-        y0 <- (y +(xj+yj)/2)
-        xlist <<- c(xlist,x0)
-        ylist <<- c(ylist,y0)
-        cat("/////////x:",x,"Y:",y,"xi:",xi,"xj:",xj,"yi:", yi,"yj:",yj,"N:",n,"\n")
+       # cat("*** Point Calculated\n")
+        x <- (x0 +(xi+yi)/2)
+        y <- (y0 +(xj+yj)/2)
+        xlist <<- c(xlist,x)
+        ylist <<- c(ylist,y)
+        #cat("*** x0:",x0,"y0:",y0,"xi:",xi,"xj:",xj,"yi:", yi,"yj:",yj,"N:",n,"\n")
     } else {
-        hilbert(x, y, yi/2, yj/2, xi/2, xj/2, n-1)
-            cat("POINT A:", n,"X:", x, "Y:", y, "\n")
-        hilbert(x+xi/2,y+xj/2,xi/2,xj/2,yi/2,yj/2,n-1)
-            cat("POINT B:", n,"X:",x,"Y:",y,"\n")
-        hilbert(x+xi/2+yi/2,y+xj/2+yj/2,xi/2,xj/2,yi/2,yj/2,n-1)
-            cat("POINT C:", n, "X:",x,"Y:", y, "\n")
-        hilbert(x+xi/2+yi, y+xj/2+yj,-yi/2,-yj/2,-xi/2,-xj/2,n-1)
-            cat("POINT D:", n, "X:",x,"Y:",y,"\n")
+        hilbert(x0, y0, yi/2, yj/2, xi/2, xj/2, n-1)
+           # cat(sprintf("point=A:::x0=%s:::y0=%s:::xi=%s:::xj=%s:::yi=%s:::yj=%s:::n-1=%s\n",
+                 #   x0, y0, yi/2, yj/2, xi/2, xj/2, n-1))
+            
+        hilbert(x0+xi/2,y0+xj/2,xi/2,xj/2,yi/2,yj/2,n-1)
+           # cat(sprintf("point=B:::x0=%s:::y0=%s:::xi=%s:::xj=%s:::yi=%s:::yj=%s:::n-1=%s\n",
+                   # x0+xi/2,y0+xj/2,xi/2,xj/2,yi/2,yj/2,n-1))
+            
+        hilbert(x0+xi/2+yi/2,y0+xj/2+yj/2,xi/2,xj/2,yi/2,yj/2,n-1)
+           # cat(sprintf("point=C:::x0=%s:::y0=%s:::xi=%s:::xj=%s:::yi=%s:::yj=%s:::n-1=%s\n",
+                  #  x0+xi/2+yi/2,y0+xj/2+yj/2,xi/2,xj/2,yi/2,yj/2,n-1))
+            
+        hilbert(x0+xi/2+yi, y0+xj/2+yj,-yi/2,-yj/2,-xi/2,-xj/2,n-1)
+           # cat(sprintf("point=D:::x0=%s:::y0=%s:::xi=%s:::xj=%s:::yi=%s:::yj=%s:::n-1=%s\n",
+                   # x0+xi/2+yi, y0+xj/2+yj,-yi/2,-yj/2,-xi/2,-xj/2,n-1))
     }
 }
 
